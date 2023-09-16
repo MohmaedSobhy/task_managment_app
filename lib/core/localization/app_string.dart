552df7a19630de0d9e83ba2cac9dc14815465e4f
Mohamed sobhy <70748430+MohmaedSobhy@users.noreply.github.com>
2023-09-17 00:36:53 +0300

    app string
    write app string

--- lib/core/localization/app_string.dart
+++ lib/core/localization/app_string.dart
@@ -9,5 +9,45 @@ abstract class AppString {
   static const String login = "LOGIN";
   static const String enterEmail = "Enter your Email";
   static const String enterPassword = "Enter your Password";
-  static const String loginSuccess = "successfully login ";
+  static const String loginSuccess = "successfully Login";
+  static const String addUser = "Add user!";
+  static const String addDepartment = "Add Department!";
+  static const String updateDepartment = "Update Department!";
+  static const String addUserDescription =
+      "Create a new user now and assign them tasks right away.";
+
+  static const String newDepartmentDescription =
+      "Create a new department now and assign manager to start the work!";
+
+  static const String updateDepartmentDescibe =
+      "Update department details and assign a new manager to continue work!";
+
+  static const String updateUserDetails = "Update User Details!";
+
+  static const String updateUserDetailsDecription =
+      "Update user details and give them a new identiy.";
+
+  static const String addTask = "Add New Task";
+  static const String addTaskDecription =
+      "Create a new task now and assign it to employess right away";
+
+  static const String name = "Name";
+  static const String phone = "Phone";
+
+  static const admin = "Admin";
+  static const String manager = "Manager";
+  static const String user = "User";
+
+  static const String assignManager = "Assigned Manager";
+
+  static const String create = "Create";
+
+  static const String departmentName = "Department name";
+
+  static const String update = "Update";
+
+  static const String title = "Title";
+  static const String description = "Description";
+
+  static const String assignedEmployee = "Assigned Employee";
 }
