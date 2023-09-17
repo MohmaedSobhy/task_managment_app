7dd475a45981f7d3804403f3ec323ab0f056fc47
Mohamed sobhy <70748430+MohmaedSobhy@users.noreply.github.com>
2023-09-17 10:51:15 +0300

    write class cacsh seure

--- lib/feature/login/controller/login_cubit_cubit.dart
+++ lib/feature/login/controller/login_cubit_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_managment_app/core/shared/shared_date.dart';
 import 'package:task_managment_app/feature/login/controller/login_cubit_state.dart';
 import '../../../core/api/api.dart';
 import '../../../core/api/end_points.dart';
@@ -23,11 +24,12 @@ class LoginCubit extends Cubit<LoginState> {
   void userLogin() {
     String email = emailController.text.toString();
     String password = passwordController.text.toString();
-    API.postMethod(baseUrl: EndPoints.login, body: {
-      'email': email,
-      'password': password,
-    }).then((response) {
-      if (checkBoxValue) {}
-    }).catchError((error) {});
+    API
+        .postMethod(baseUrl: EndPoints.login, body: {
+          'email': email,
+          'password': password,
+        })
+        .then((response) {})
+        .catchError((error) {});
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:task_managment_app/core/shared/shared_date.dart';
 import 'task_managment.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper.init();
   runApp(const TaskMangmentApp());
 }
